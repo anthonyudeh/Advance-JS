633cfd07bb2b2af38e462fda3ecb119793fe707f
Anthony Udeh Chinaza <anthonyudehchinaza@gmail.com>
2023-07-19 09:32:29 -0700

    arrays and functions

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ console.log(`I am ${ageII} years old :)`)
 const ageIII = function (birthyear) {
     return 2055 - birthyear
 }
-const ageIV = ageIII(1983); 
+const ageIV = ageIII(1983);
 console.log(ageIV);
 
 //expressions produces values
@@ -84,8 +84,8 @@ console.log(ageV)
 const yearsUntilRetirement = birthyear => {
     const dateOfBirth = 2057 - birthyear;
     const numberOfworkingYears = 10;
-    const retirementAge = dateOfBirth - numberOfworkingYears; 
-    return retirementAge; 
+    const retirementAge = dateOfBirth - numberOfworkingYears;
+    return retirementAge;
 }
 
 const retirementAgeInfo = yearsUntilRetirement(1992)
@@ -94,11 +94,11 @@ console.log(`Jonas retired at the age of ${retirementAgeInfo} years :)`)
 const yearsUntilRetirementII = (birthyear, firstName) => {
     const dateOfBirth = 2057 - birthyear;
     const numberOfworkingYears = 10;
-    const retirementAgeII = dateOfBirth - numberOfworkingYears; 
-    return `${firstName} retired at ${retirementAgeII}`; 
+    const retirementAgeII = dateOfBirth - numberOfworkingYears;
+    return `${firstName} retired at ${retirementAgeII}`;
 }
 
-const retirementAgeInfoII = yearsUntilRetirementII (1993, 'Bob')
+const retirementAgeInfoII = yearsUntilRetirementII(1993, 'Bob')
 console.log(retirementAgeInfoII)
 
 // The 'this' key word can not be used in the arrow function
@@ -111,22 +111,166 @@ console.log(retirementAgeInfoII)
 
 //frui machine 
 
-function cutFruitPieces(fruit){
+function cutFruitPieces(fruit) {
     return fruit * 4
 }
 
- function fruitMainProcessor(mangoes, bananas){
-    
+function fruitMainProcessor(mangoes, bananas) {
+
     //embedded function
     const cutMangoes = cutFruitPieces(mangoes); 
-    const cutBananas = cutFruitPieces(bananas); 
-    
+    const cutBananas = cutFruitPieces(bananas);
+
     const myJuiceMix = `the food processor is going to mix ${cutMangoes} mangoes and ${cutBananas} bananas`
-     
- }
- const diffJuiceMix1 = fruitMainProcessor(20,30)
+    return myJuiceMix
+}
+const diffJuiceMix1 = fruitMainProcessor(20, 30)
 console.log(diffJuiceMix1)
 
-const diffJuiceMix2 = fruitMainProcessor(2,9)
+const diffJuiceMix2 = fruitMainProcessor(2, 9)
 console.log(diffJuiceMix2)
 
+//example 2 
+
+
+const companyAge = function (birthyear) {
+    return 2005 - birthyear
+}
+const businessAge = function (birthyear, businessName) {
+
+    const myCompanyAge = companyAge(birthyear);
+
+    if (myCompanyAge > 0) {
+        return `the company ${businessName}, is ${myCompanyAge} years old`
+    } else {
+        return -1
+    }
+
+}
+console.log(businessAge(1992, 'Alvoant')); 
+//function declaration you can invoke the function before declaring the function
+//function expression is stored in a variable 
+//arrow function has no this key word   
+
+
+//coding chanllenge.... 
+
+ const calcAverage1 = (Data1, Data2, Data3, Data4, Data5, Data6) => {
+
+    const dolphinsAvg = (Data1 + Data2 + Data3)/ 3;
+    const koalasAvg = (Data4 + Data5 + Data6) / 3;
+    return `the average for the Dolphins is ${dolphinsAvg} and the average for the koalas is ${koalasAvg}`
+ }
+
+const firstData = calcAverage1(12,24,45,3345,498,100)
+console.log(firstData)
+
+ function checkWinner (avgDolphins, avgKoalas){
+    if(avgDolphins >= avgKoalas*2){
+        console.log(`the dolphines wins ${avgDolphins} vs ${avgKoalas}`)
+    }else if(avgKoalas >= avgDolphins * 2){
+        console.log(`the koalas wins ${avgKoalas} vs ${avgDolphins}`)
+    }else{
+        console.log(`no team wins`)
+    }
+
+ }
+checkWinner(firstData)
+
+//Data Structure --->  ARRAYS this a type of data structure and works like a big container that carries data we can referrence later. 
+//there are two popular types of data structures arrays and object
+
+const friends = ['tunde', 'james', 'peter']; //literal syntax
+console.log(friends[0]); //arrays are zero based 
+
+//another way of creating an array 
+
+const years = new Array(1991,1992,1993,1994); //using array function and using the 'new' key word 
+console.log(years);
+
+
+//number of elements in the array 
+console.log(friends.length); 
+
+//last element in the array 
+console.log(friends[friends.length -1]); //friends.length-1 is an expression not a statement
+
+//replace elements in the array 
+friends[2] = 'Kev'; 
+console.log(friends); 
+
+//holds different values 
+const firstName = 'Jonas'; 
+const jonas = [firstName, 'Schmedtmann', 2037-1981, 'teachers', friends];
+console.log(jonas);
+
+
+//simple excerise 
+const newCalcAge = function(birthyear){
+    return 2037 - birthyear; 
+}
+
+const ages = [1991, 1901, 1984, 2005];
+
+const firstAge = newCalcAge(ages[0]);
+const secondAge = newCalcAge(ages[1]);
+const thirdAge = newCalcAge(ages[ages.length -1]);
+console.log(firstAge, secondAge, thirdAge); 
+
+const newAges = [newCalcAge(ages[0]), newCalcAge(ages[1]), newCalcAge(ages[ages.length - 1])];
+console.log(newAges);
+
+
+//ARRAY METHODS 
+const newFriends = ['Emma', 'Dammy', 'JR.Light']
+
+//push method -> we want to push an element to an array
+newFriends.push('Jay'); //push function , it returns the lenght of the new array
+console.log(newFriends); 
+
+//unshift method -> we want to add an element at the beginning of the array 
+newFriends.unshift('Grey')
+console.log(newFriends); 
+
+//pop method -> we want to remove the last element 
+newFriends.pop();  // it returns the removed elements 
+const popped = newFriends.pop();
+console.log(popped); 
+console.log(newFriends);
+
+//shift method -> we want to remove the first element 
+newFriends.shift(); 
+console.log(newFriends);
+
+//we want to know the position of an element 
+console.log(newFriends.indexOf('Dammy'));
+
+console.log(newFriends.indexOf('JR.Light')); //if you want to know the position of an element that is not in the array it will give you minus one
+
+//ES06 'includes method' (modern method) 
+console.log(newFriends.includes('JR.Light'));
+console.log(newFriends.indexOf('Dammy')); //this does not accept coertion ;
+
+if(newFriends.includes('Peter')){
+    console.log `you have a friend named peter`
+}else{
+    console.log `you do not have a friend named peter`
+}
+
+
+//code chanllege 
+
+const calcTip = function(billValue){
+    if(billValue >= 50 && billValue <= 300){
+          return (billValue * (15/100)) + billValue;
+    }else{
+        return (billValue * (20/100)) + billValue;
+    }
+}
+console.log(calcTip(100));
+
+const tips = [calcTip(125) - 125, calcTip(555)-555, calcTip(44)-44]
+console.log(tips)
+
+const bills = [calcTip(125), calcTip(555), calcTip(44)];
+console.log(bills)
